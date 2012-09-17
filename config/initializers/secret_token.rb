691cb953e3e2b49c1a5143b13deee602fe8dbc89
usman427 <usman.nufast@gmail.com>
2012-09-17 12:57:37 +0500

    All files, first upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ems::Application.config.secret_token = '47c2dfebcf518c0f568e0cbe44b43d914d4423a3bd898c0ecf87c0d071f5ebee0747a3d9a42a283053861e0392f4e044fadfe2a18b2df0199f36582e56ba9c07'
